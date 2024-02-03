f1ed7f8f6cc66bc13f4379ee9e374cdac151b274
srpanwar145 <158435775+srpanwar145@users.noreply.github.com>
2024-02-03 10:38:14 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/srpanwar145/Gangster-Baby-Renamer-BOT /Gangster-Baby-Renamer-BOT
+cd /MissPerfectURLBOT
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python bot.py
